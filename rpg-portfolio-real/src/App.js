43d60c614ebce006a489f5054905c6245e982aaa
kmooney225 <kmooney225@gmail.com>
2023-01-25 14:18:50 -0700

    change size of screen

--- rpg-portfolio-real/src/App.js
+++ rpg-portfolio-real/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div style= {{display: 'inline-block', position: 'relative'}}>
     <div id= 'overlappingDiv' style= {{'backgroundColor': 'black', position: 'absolute', top: 0, bottom: 0, right: 0, left: 0, opacity: 0, 'pointerEvents': 'none'}}>ye</div>
-      <Scene width={800} height={700}/>
+      <Scene width={1000} height={700}/>
     </div>
   );
 }
